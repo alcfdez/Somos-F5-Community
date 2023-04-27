43fb72d00d92229c88f20169be54268054680496
alcfdez <alcfdez@hotmail.com>
2023-04-27 09:01:03 +0200

    repare header Login

--- src/main/frontend/src/components/HeaderLogin.vue
+++ src/main/frontend/src/components/HeaderLogin.vue
@@ -1,18 +1,34 @@
-<script setup>
-</script>
-
+<script></script>
 <template>
-
-<body id="HeaderLogin"></body>
-
+  <div class="headerLogin">
+    <a href="/" aria-current="page" class="headerLogo"></a>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss">
+@use "@/scss/colors" as c;
+@use "@/scss/mixins" as m;
 
-#HeaderLogin{
-    width: 100%;
-    height: 20%;
-    background-color: #FED2C0;
+.headerLogin {
+  left: 0%;
+  top: 0%;
+  right: 0%;
+  bottom: 0%;
+  display: flex;
+  height: 5rem;
+  -webkit-box-pack: center;
+  justify-content: center;
+  background-color: map-get(c.$colors, "light-orange");
+
+  .headerLogo {
+    background-image: url("https://uploads-ssl.webflow.com/62e2b7b9c42bdda27c83d493/6329c1bc9e1976ae0ada64b0_somos-f5.svg");
+    background-size: contain;
+    background-position: 0% 50%;
+    background-repeat: no-repeat;
+    width: 9rem;
+    height: 4rem;
+    margin: auto;
+    margin-left: 1vw;
+  }
 }
-
 </style>
